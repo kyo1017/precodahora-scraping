b911a350d9fe18de3bfbba50a5d7a143e8eaf1d6
kyo1017 <developestock88@gmail.com>
2021-11-06 18:53:30 +0300

    improve the speed

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
-import requests, time
+import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+
+keyword = input('\nKeyword: ')
+zipCode = input('\nZip Code: ')
 
 name = []
 barCode = []
@@ -24,71 +25,54 @@ headers = {
     'X-Requested-With': 'XMLHttpRequest'
 }
 
-keyword = input('\nKeyword: ')
-zipCode = input('\nZip Code: ')
-
-payload = {
-    'termo': keyword,
-    'horas': 72,
-    'latitude': -12.97111,
-    'longitude': -38.51083,
-    'raio': 15,
-    'pagina': 1,
-    'ordenar': 'preco.asc',
+locPayload = {
+    'cep': zipCode
 }
 
-res = session.post('https://precodahora.ba.gov.br/produtos/', headers=headers, cookies=cookie, data=payload).json()
+try:
 
-if res['codigo'] == 80:
-    
-    products = res['resultado']
+    locRes = session.post('https://precodahora.ba.gov.br/geolocation/', headers=headers, cookies=cookie, data=locPayload).json()
 
-    for i, product in enumerate(products):
-        if i > 10:
-            break
-        name.append(product['produto']['descricao'])
-        barCode.append(product['produto']['gtin'])
-        price.append(product['produto']['precoBruto'])
-        issue.append(product['produto']['data'])
-        df = product['estabelecimento']['endLogradouro'] + ' ' + product['estabelecimento']['endNumero'] + ' ' + product['estabelecimento']['bairro'] + ' ' + product['estabelecimento']['cep'] + ', ' + product['estabelecimento']['municipio']
-        address.append(df)
+    if locRes['codigo'] == 80:
 
-    chromeOptions = webdriver.ChromeOptions()
-    chromeOptions.add_argument('--headless')
-    chromeOptions.add_experimental_option('excludeSwitches', ['enable-logging'])
-    chromeOptions.add_argument('--log-level=3')
-    browser = webdriver.Chrome(ChromeDriverManager().install(), options=chromeOptions)
-    browser.get('https://precodahora.ba.gov.br/')
+        lat = locRes['lat']
+        lon = locRes['lon']
 
-    browser.find_element_by_id('fake-sbar').click()
-    browser.find_element_by_id('top-sbar').send_keys(keyword)
-    browser.find_element_by_class_name('btn-top-sbar').click()
+        proPayload = {
+            'termo': keyword,
+            'horas': 72,
+            'latitude': lat,
+            'longitude': lon,
+            'raio': 15,
+            'pagina': 1,
+            'ordenar': 'preco.asc',
+        }
 
-    browser.find_element_by_css_selector('.location-box button').click()
-    time.sleep(1)
-    browser.find_element_by_id('add-address').click()
-    time.sleep(1)
-    browser.find_element_by_id('my-zip').send_keys(zipCode)
-    browser.find_element_by_id('sel-cep').click()
-    time.sleep(5)
+        proRes = session.post('https://precodahora.ba.gov.br/produtos/', headers=headers, cookies=cookie, data=proPayload).json()
 
-    products = browser.find_elements_by_class_name('item-list')
+        if proRes['codigo'] == 80:
+            
+            products = proRes['resultado']
 
-    for i, product in enumerate(products):
-        if i > 10:
-            break
-        data = product.find_elements_by_css_selector('.flex-item2 div')
-        if 'Km' in data[5].text:
-            distance.append(data[5].text)
+            for i, product in enumerate(products):
+                if i > 10:
+                    break
+                name.append(product['produto']['descricao'])
+                barCode.append(product['produto']['gtin'])
+                price.append(product['produto']['precoBruto'])
+                issue.append(product['produto']['data'])
+                address.append(product['estabelecimento']['endLogradouro'] + ' ' + product['estabelecimento']['endNumero'] + ' ' + product['estabelecimento']['bairro'] + ' ' + product['estabelecimento']['cep'] + ', ' + product['estabelecimento']['municipio'])
+                distance.append(product['estabelecimento']['distancia'])
+
+            dict = {'Product Name': name, 'Product Bar Code': barCode, 'Price': price, 'Issue Date': issue, 'Address': address, 'Approximate Distance': distance} 
+            df = pd.DataFrame(dict)
+            print("\n", df)
+            df.to_csv(keyword+'.csv', encoding='utf-8-sig')
+
         else:
-            distance.append(data[6].text)
+            print(proRes['descricao'])
+    else:
+        print(locRes['descricao'])
 
-    browser.quit()
-
-    dict = {'Product Name': name, 'Product Bar Code': barCode, 'Price': price, 'Issue Date': issue, 'Address': address, 'Approximate Distance': distance} 
-    df = pd.DataFrame(dict)
-    print("\n", df)
-    df.to_csv(keyword+'.csv', encoding='utf-8-sig')
-
-else:
-    print(res['descricao'])
+except:
+    print('\nPlease enter your zip code correctly (*****-***)')
