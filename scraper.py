269858f1c1fcae6acd4404bd3141f85ef24df7ab
kyo1017 <developestock88@gmail.com>
2021-11-06 15:44:59 +0300

    fix about cookie and token

--- scraper.py
+++ scraper.py
@@ -1,13 +1,31 @@
-import requests
+import requests, time
 import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-keyword = input('\nKeyword: ')
+name = []
+barCode = []
+price = []
+issue = []
+address = []
+distance = []
+
+session = requests.Session()
+r = session.get('https://precodahora.ba.gov.br/produtos/')
+
+cookie = session.cookies.get_dict()
+
+soup = BeautifulSoup(r.text, features='lxml')
+csrf_token = soup.select_one('meta[id="validate"]')['data-id']
 
 headers = {
-    'Cookie': 'session=eyJjc3JmX3Rva2VuIjoiYjU3MjY0NjJiMTIyNDRhMGQyMTM0ZGQzZWZmZjk3NmE0N2VhODAzYiJ9.YYS90g.sukA-o3wSb7Z7UDnu-vGMlSH8s4; token=7druNGgoRl4vv-CuRZCbY0-uEQLOHYHpbFcSxiMatVn0xFq51992W0fUTAaEoA4rfOH15Q0elMSRDUEUqRDFRDvop84',
-    'X-CSRFToken': 'ImI1NzI2NDYyYjEyMjQ0YTBkMjEzNGRkM2VmZmY5NzZhNDdlYTgwM2Ii.YYS98Q.cc3irB4uWUfaWZyOWocCiLGqkGU',
+    'X-CSRFToken': csrf_token,
     'X-Requested-With': 'XMLHttpRequest'
 }
+
+keyword = input('\nKeyword: ')
+zipCode = input('\nZip Code: ')
 
 payload = {
     'termo': keyword,
@@ -19,38 +37,58 @@ payload = {
     'ordenar': 'preco.asc',
 }
 
-res = requests.post('https://precodahora.ba.gov.br/produtos/', headers=headers, data=payload).json()
+res = session.post('https://precodahora.ba.gov.br/produtos/', headers=headers, cookies=cookie, data=payload).json()
 
 if res['codigo'] == 80:
-
+    
     products = res['resultado']
 
-    name = []
-    barCode = []
-    price = []
-    issue = []
-    address = []
-    distance = []
-
     for i, product in enumerate(products):
-
         if i > 10:
             break
-
         name.append(product['produto']['descricao'])
         barCode.append(product['produto']['gtin'])
         price.append(product['produto']['precoBruto'])
         issue.append(product['produto']['data'])
-        address.append(product['estabelecimento']['endLogradouro'])
-        distance.append(product['estabelecimento']['distancia'])
+        df = product['estabelecimento']['endLogradouro'] + ' ' + product['estabelecimento']['endNumero'] + ' ' + product['estabelecimento']['bairro'] + ' ' + product['estabelecimento']['cep'] + ', ' + product['estabelecimento']['municipio']
+        address.append(df)
+
+    chromeOptions = webdriver.ChromeOptions()
+    chromeOptions.add_argument('--headless')
+    chromeOptions.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chromeOptions.add_argument('--log-level=3')
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=chromeOptions)
+    browser.get('https://precodahora.ba.gov.br/')
+
+    browser.find_element_by_id('fake-sbar').click()
+    browser.find_element_by_id('top-sbar').send_keys(keyword)
+    browser.find_element_by_class_name('btn-top-sbar').click()
+
+    browser.find_element_by_css_selector('.location-box button').click()
+    time.sleep(1)
+    browser.find_element_by_id('add-address').click()
+    time.sleep(1)
+    browser.find_element_by_id('my-zip').send_keys(zipCode)
+    browser.find_element_by_id('sel-cep').click()
+    time.sleep(5)
+
+    products = browser.find_elements_by_class_name('item-list')
+
+    for i, product in enumerate(products):
+        if i > 10:
+            break
+        data = product.find_elements_by_css_selector('.flex-item2 div')
+        if 'Km' in data[5].text:
+            distance.append(data[5].text)
+        else:
+            distance.append(data[6].text)
+
+    browser.quit()
 
     dict = {'Product Name': name, 'Product Bar Code': barCode, 'Price': price, 'Issue Date': issue, 'Address': address, 'Approximate Distance': distance} 
-
     df = pd.DataFrame(dict)
-
     print("\n", df)
-
     df.to_csv(keyword+'.csv', encoding='utf-8-sig')
 
 else:
-    print("\nNo Result")
+    print(res['descricao'])
